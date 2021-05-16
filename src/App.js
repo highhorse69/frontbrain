4f9e946dba98ab48333d71acd1fe790a35920b68
Rohan Sahani <rohan.sahani.m97@gmail.com>
2021-05-16 14:28:25 +0530

    k

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ class App extends Component {
   onClickChange = ()  => {
 
    this.setState({imgurl:this.state.input});
-  fetch(' https://fathomless-sierra-52069.herokuapp.com/imageurl',{
+  fetch('https://fathomless-sierra-52069.herokuapp.com/imageurl',{
         method: 'post',
         headers:{'Content-Type': 'application/json'},
         body:JSON.stringify({
